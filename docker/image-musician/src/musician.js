925c4b78009abeffe098d9ca90f286e4dff1f8db
Mentor Reka <MentorReka@M2O.local>
2018-05-29 21:29:43 +0200

    Refactor the musician code

--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -1,14 +1,8 @@
-/**
- * @author Mentor Reka & Kamil Amrani
- * @brief Musician
- * @date 07.05.2018
- */
-// From nodejs.org
 const dgram = require("dgram");
 const server = dgram.createSocket("udp4");
-server.set
-
 const protocol = require("./protocol");
+const uuid = require("uuid");
+const moment = require("moment");
 
 var instruments = new Map();
 instruments.set("piano", protocol.PIANO);
@@ -17,21 +11,26 @@ instruments.set("flute", protocol.FLUTE);
 instruments.set("violin", protocol.VIOLIN);
 instruments.set("drum", protocol.DRUM);
 
-
 function Musician(instrument) {
     this.instrument = instrument;
 
-    Musician.prototype.sendSound = function() {
-        var sound = instruments.get(instrument);
-        var payload = JSON.stringify(sound);
+    // Otherwise for every data received from musician, we generate wrongly a new uuid
+    var data = {
+        uuid: uuid(),
+        instrument: instrument
+    }
+
+    Musician.prototype.sendMessage = function() {
+        data.activeSince = moment();
+        var payload = JSON.stringify(data);
         var message = new Buffer(payload);
-        server.send(message, protocol.PORT, "localhost",function(err, bytes) {
-            console.log("Sending payload: " + payload + " via port " + protocol.PORT);
+        server.send(message, 0, message.length, protocol.PORT, protocol.MULTICAST_IP, function(err, bytes) {
+            console.log("Sending payload: " + payload);
         });
     }
 
     // send the sound every given interval
-    setInterval(this.sendSound.bind(this), protocol.MUSICIAN_INTERVAL);
+    setInterval(this.sendMessage.bind(this), protocol.MUSICIAN_INTERVAL);
 }
 
 var instrument = process.argv[2];
--- docker/image-musician/src/protocol.js
+++ docker/image-musician/src/protocol.js
@@ -1,9 +1,6 @@
-// IP configuration
-exports.PROTOCOL_MULTICAST_ADDRESS = "239.255.22.5";
-exports.PROTOCOL_AUDITOR_ADDRESS = "127.0.0.1";
+exports.MULTICAST_IP = "239.255.22.15";
 exports.PORT = 9907;
 
-// Musician parameters
 exports.PIANO = "ti-ta-ti";
 exports.TRUMPER = "pouet";
 exports.FLUTE = "trulu";
